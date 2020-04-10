f887a33cb308272eab14c948f636f11f26eb9da0
Young <abiodunadebambo44@gmail.com>
2020-04-10 01:48:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var button= document.querySelector(".toggle");
+var container= document.querySelector(".nav-container");
+var dropItems = document.querySelectorAll(".dropitems");
+var plus = document.querySelectorAll(".plus");
+
+
+button.addEventListener("click", function(){
+    container.classList.toggle("new_nav-container");
+    // toggle the burger
+    button.classList.toggle("toggled")
+})
+
+function addClass(){
+	Array.prototype.slice.call(plus).forEach(function(i){
+		i.addEventListener("click", function(clicking){
+            console.log(clicking.target.parentNode.nextElementSibling);
+			clicking.target.parentNode.nextElementSibling.classList.toggle("second-order_dropdown");
+		});
+	});
+}
+addClass();
